a158299cdcb4c9472530a18dff06b9166b2bb0cc
agua-2025 <vivendamirassol@gmail.com>
2025-05-26 16:46:25 -0400

    Projeto corrigido com react-scripts

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-10 text-center text-2xl text-blue-600">
+      Sistema AquaGest funcionando com sucesso!
+    </div>
+  );
+}
+
+export default App;
